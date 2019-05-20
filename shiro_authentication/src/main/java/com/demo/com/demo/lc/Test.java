a83cc27cd382f83b6ff0223becd60ba07f5da196
fengh <fenghuo0203@outlook.com>
2019-05-20 12:12:30 +0800

    lc

---
+++ shiro_authentication/src/main/java/com/demo/com/demo/lc/Test.java
@@ -0,0 +1,11 @@
+package com.demo.com.demo.lc;
+
+/**
+ * @author 李畅
+ * @date 2019/5/20
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("李博涵是阴阳师");
+    }
+}
